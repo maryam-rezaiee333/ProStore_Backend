5874fbc1c0c0c7061667b26b2b2be4016387f0db
maryam-rezaiee333 <m.rezaiee136@gmail.com>
2026-04-15 15:03:36 -0700

    The request file of UpdateProduct added.

---
+++ app/Http/Requests/UpdateProductRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+            'name'=> ['required' ,'string','min:3', Rule::unique('products','name')],
+            'price'=> 'required|numaric|min:10|max:19000',
+            'stock'=> 'required|integer|min:1|max:200',
+            'brand'=> 'required|string',
+            'description'=> 'required|string|min:10',
+            'category'=> 'required|string|min:10',
+        ];
+    }
+}
